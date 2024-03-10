a3c7ced52541c2305595519aa2bc3047e018de91
ZlatasGit <zlatayan.des@gmail.com>
2024-03-10 21:06:18 +0100

    added new project, created console application

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter an integer: ");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+if(number<0){
+    Console.WriteLine(number+" is smaller than zero");
+} else if(number>0){
+    Console.WriteLine(number+" is greater than zero");
+} else {
+    Console.WriteLine("entered number is zero");
+}
